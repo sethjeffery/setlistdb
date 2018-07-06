14c7f77d5a34b50a1471f4b69ba1b85e4f6bd20c
Seth Jeffery <seth@sethjeffery.com>
2018-07-06 09:09:43 +0100

    better sign in/out

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    p request.env["omniauth.auth"]
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    p @user
     sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
     set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
   end
 
   def failure
     redirect_to root_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions',
   }
+
+  devise_scope :user do
+    delete "/logout" => "users/sessions#destroy", as: :logout
+  end
 
   root to: "home#index"
 
