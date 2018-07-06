57fe0fa604c4d36393066792cc4b330484eea531
Seth Jeffery <seth@sethjeffery.com>
2018-07-06 08:08:35 +0100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_05_211653) do
+ActiveRecord::Schema.define(version: 2018_07_06_070219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2018_07_05_211653) do
     t.string "provider"
     t.string "uid"
     t.string "image"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
