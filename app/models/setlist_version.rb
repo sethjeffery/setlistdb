cf4da4950963a6f1e943216ea1ee5e56273ca81d
Seth Jeffery <seth@sethjeffery.com>
2018-07-27 20:45:55 +0100

    notes may not be defined yet

--- app/models/setlist_version.rb
+++ app/models/setlist_version.rb
@@ -18,7 +18,7 @@ class SetlistVersion < ApplicationRecord
     self.key ||= version.key
     self.year ||= version.year
     self.lang ||= version.lang
-    self.notes ||= version.notes
+    self.notes ||= version.notes if defined?(:notes=)
 
     self.artist_names = version.artist_names if artist_names.blank?
     self.composer_names = version.composer_names if composer_names.blank?
